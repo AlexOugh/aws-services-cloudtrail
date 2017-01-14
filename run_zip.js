31d077968c64b86268d32f3406895ddef10324f8
Alex Ough <alex.ough@sungardas.com>
2017-01-13 18:26:39 -0600

    added missing files in zip and extended timeout in lambda

--- run_zip.js
+++ run_zip.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
 vfs = require('vinyl-fs'),
 zip = require('gulp-zip');
 
-vfs.src(['index.js', 'node_modules/aws-services-lib/flow_controller.js', 'node_modules/aws-services-lib/*/*.js', 'json/*.json'],{cwd:'./src', base:'./src'})
+vfs.src(['index.js', 'node_modules/aws-services-lib/flow_controller.js', 'node_modules/aws-services-lib/*/*', 'node_modules/aws-services-lib/node_modules/*/*', 'json/*.json'],{cwd:'./src', base:'./src'})
 .pipe(zip('cloudtrail.zip'))
 .pipe(gulp.dest('.'))
 .on('end', function(err, data) {
